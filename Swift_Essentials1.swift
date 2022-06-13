630f1373ddb0e04799a37f810e6c3155264fa345
Aaron Ramirez <amramirez1222@gmail.com>
2022-06-12 15:14:08 -1000

    Part 1 of essentials
    coded part one of the code for landmark

---
+++ Swift_Essentials1.swift
@@ -0,0 +1,17 @@
+//
+//  Smart_MusicApp.swift
+//  Smart Music
+//
+//  Created by Aaron Ramirez on 6/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Smart_MusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
